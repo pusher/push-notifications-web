4ed36e4595935633dbf36c4cef92057a9c6e810f
minaorangina <mina@pusher.com>
2019-04-18 11:39:06 +0100

    names global object PusherPushNotifications

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,11 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default [
-  // browser-friendly UMD build
   {
     input: 'src/push-notifications.js',
     output: {
-      name: 'PushNotifications',
-      file: 'dist/push-notifications.js',
+      name: 'PusherPushNotifications',
+      file: 'push-notifications.js',
       format: 'umd'
     },
     plugins: [resolve(), commonjs()]
