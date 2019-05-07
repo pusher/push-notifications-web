b2a396c2c03bd5344ab5ecfcd36497305b02e270
Jonathan Lloyd <jonathan@thisisjonathan.com>
2019-05-07 16:24:03 +0100

    Add end-to-end test for device registration

--- end-to-end-tests/sdk-start.test.js
+++ end-to-end-tests/sdk-start.test.js
@@ -11,28 +11,73 @@ beforeAll(() => {
     .then(() => createChromeWebDriver())
     .then(driver => {
       chromeDriver = driver;
-    })
+    });
 });
 
-
-test('Example test - page title', async () => {
+test('SDK should register a device with errol', async () => {
   await chromeDriver.get('http://localhost:3000');
   await chromeDriver.wait(() => {
-    return chromeDriver.getTitle()
-      .then(title => title.includes('Test Page'))
+    return chromeDriver.getTitle().then(title => title.includes('Test Page'));
   }, 2000);
 
-  const result = await chromeDriver.executeScript(() => {
-    return document.title;
+  const initialDeviceId = await chromeDriver.executeAsyncScript(() => {
+    var callback = arguments[arguments.length - 1];
+
+    const instanceId = 'deadc0de-2ce6-46e3-ad9a-5c02d0ab119b';
+    let beamsClient;
+    return PusherPushNotifications.init({ instanceId })
+      .then(c => (beamsClient = c))
+      .then(() => beamsClient.start())
+      .then(() => callback(beamsClient.deviceId))
+      .catch(e => callback(e.message));
   });
 
-  expect(result).toBe('Push Notifications Web - Test Page');
+  expect(initialDeviceId).toContain('web-');
 });
 
+test('SDK should remember the device ID', async () => {
+  await chromeDriver.get('http://localhost:3000');
+  await chromeDriver.wait(() => {
+    return chromeDriver.getTitle().then(title => title.includes('Test Page'));
+  }, 2000);
+
+  const initialDeviceId = await chromeDriver.executeAsyncScript(() => {
+    var callback = arguments[arguments.length - 1];
+
+    const instanceId = 'deadc0de-2ce6-46e3-ad9a-5c02d0ab119b';
+    let beamsClient;
+    return PusherPushNotifications.init({ instanceId })
+      .then(c => (beamsClient = c))
+      .then(() => beamsClient.start())
+      .then(() => callback(beamsClient.deviceId))
+      .catch(e => callback(e.message));
+  });
+
+  await chromeDriver.get('http://localhost:3000');
+  await chromeDriver.wait(() => {
+    return chromeDriver.getTitle().then(title => title.includes('Test Page'));
+  }, 2000);
+
+  const reloadedDeviceId = await chromeDriver.executeAsyncScript(() => {
+    var callback = arguments[arguments.length - 1];
+
+    const instanceId = 'deadc0de-2ce6-46e3-ad9a-5c02d0ab119b';
+    let beamsClient;
+    return PusherPushNotifications.init({ instanceId })
+      .then(c => (beamsClient = c))
+      .then(() => beamsClient.start())
+      .then(() => callback(beamsClient.deviceId))
+      .catch(e => callback(e.message));
+  });
+
+  expect(reloadedDeviceId).toBe(initialDeviceId);
+});
 
 afterAll(() => {
   if (killServer) {
     killServer();
   }
+  if (chromeDriver) {
+    chromeDriver.quit();
+  }
 });
-
