33b53527ed796db7a35af9b69105da93aa40442f
Jonathan Lloyd <jonathan@thisisjonathan.com>
2019-05-28 15:02:41 +0100

    Add support for opening deep_links on click

--- src/service-worker.js
+++ src/service-worker.js
@@ -6,6 +6,12 @@ self.addEventListener('push', function(e) {
   const icon = payload.notification.icon;
   const data = payload.data || {};
 
+  if (payload.notification.deep_link) {
+    // Copying the deep_link into the data payload so that it can
+    // be accessed in the notificationclick handler.
+    data.pusher.deep_link = payload.notification.deep_link;
+  }
+
   const options = {
     title,
     body,
@@ -14,4 +20,12 @@ self.addEventListener('push', function(e) {
   };
 
   e.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  const deep_link = event.notification.data.pusher.deep_link;
+  if (deep_link) {
+    event.waitUntil(clients.openWindow(deep_link));
+  }
+  event.notification.close();
 });
