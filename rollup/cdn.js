3afcdda936c889ae1d02f0ae626828587b259d6c
Luis Fonseca <luis@pusher.com>
2019-06-24 15:47:20 +0100

    Add json rollup to import sdk version from package.json

--- rollup/cdn.js
+++ rollup/cdn.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import json from 'rollup-plugin-json';
 
 export default [
   {
@@ -11,6 +12,7 @@ export default [
       format: 'iife',
     },
     plugins: [
+      json(),
       resolve(),
       commonjs(),
       babel({
--- rollup/esm.js
+++ rollup/esm.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import json from 'rollup-plugin-json';
 
 export default [
   {
@@ -10,6 +11,7 @@ export default [
       format: 'esm',
     },
     plugins: [
+      json(),
       resolve(),
       commonjs(),
       babel({
