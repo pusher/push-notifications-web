a0784450c79559f327b86cf15d1e3445156d27f5
Jonathan Lloyd <jonathan@thisisjonathan.com>
2020-06-26 11:28:15 +0100

    Add TypeScript type definitions

---
+++ index.d.ts
@@ -0,0 +1,46 @@
+declare module '@pusher/push-notifications-web' {
+  export interface TokenProviderResponse {
+    token: string;
+  }
+  export interface ITokenProvider {
+    fetchToken(userId: string): Promise<TokenProviderResponse>;
+  }
+
+  interface TokenProviderOptions {
+    url: string;
+    queryParams?: { [key: string]: any };
+    headers?: { [key: string]: string };
+  }
+
+  class TokenProvider implements ITokenProvider {
+    constructor(options: TokenProviderOptions);
+    fetchToken(userId: string): Promise<TokenProviderResponse>;
+  }
+
+  class PushNotificationsInstance {
+    instanceId: string;
+    deviceId: string;
+    userId: string;
+
+    start(): Promise<PushNotificationsInstance>;
+    addDeviceInterest(interest: string): Promise<undefined>;
+    removeDeviceInterest(interest: string): Promise<undefined>;
+    getDeviceInterests(): Proimise<Array<string>>;
+    setDeviceInterests(interests: Array<string>): Promise<undefined>;
+    clearDeviceInterests(): Promise<undefined>;
+    setUserId(
+      userId: string,
+      tokenProvider: ITokenProvider
+    ): Promise<undefined>;
+    stop(): Promise<undefined>;
+    clearAllState(): Promise<undefined>;
+  }
+
+  interface InitConfig {
+    instanceId: string;
+    serviceWorkerRegistration?: ServiceWorkerRegistration;
+    endpointOverride?: string;
+  }
+
+  export function init(config: InitConfig): Promise<PushNotificationsInstance>;
+}
