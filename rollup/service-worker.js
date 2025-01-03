7a784edb07612a69e13d39552f24645d1dd0962c
Heinrich Vogel <heinvogel@gmail.com>
2025-01-03 09:50:31 +0100

    Migrate Rollup Json plugin to new version

--- rollup/service-worker.js
+++ rollup/service-worker.js
@@ -1,7 +1,7 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { babel } from '@rollup/plugin-babel';
-import json from 'rollup-plugin-json';
+import json from '@rollup/plugin-json';
 
 export default [
   {
